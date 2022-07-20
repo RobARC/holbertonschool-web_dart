16a4566a9c3e06510fc19298125f7c113c580aed
RobARC <rrondoc@gmail.com>
2022-07-20 18:25:44 -0500

    Uploading task 1

---
+++ 0x01-dart-function/1-concat_strings.dart
@@ -0,0 +1,7 @@
+// Use string interpolation to concatenate two strings
+// Could also str1 + ' ' + str2 or $str1 $str2
+
+String concatStr(String str1, String str2){
+    String blank = " ";
+    return "$str1$blank$str2";
+}
