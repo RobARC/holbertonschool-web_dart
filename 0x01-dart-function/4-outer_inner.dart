f84b3c0640c4e06c3a3d04a7a7bb4ec3c45f0ef8
RobARC <rrondoc@gmail.com>
2022-07-20 20:02:29 -0500

    Uploading task 4

---
+++ 0x01-dart-function/4-outer_inner.dart
@@ -0,0 +1,10 @@
+void outer(String name, String id){
+    String inner(String name, String id) {
+    // Split name by space - first name is first index of split array
+    String first = name.split(" ")[0];
+    // Find space, then find substring of first letter to find last initial
+    String last = name.substring(name.indexOf(" ") + 1, name.indexOf(" ") + 2);
+    return "Hello Agent $last.$first your id is $id";
+  }
+  print(inner(name, id));
+}
